736f12d7274ff797518d08ee0a69174235c67e90
Peter Steenbergen <peter@3ws.nl>
2016-03-12 20:54:57 +0100

    ServiceProvider test

---
+++ tests/ServiceProviderTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace PeterIcebear\Tests\Mollie;
+
+use GrahamCampbell\TestBenchCore\ServiceProviderTrait;
+
+class ServiceProviderTest extends AbstractTestCase
+{
+    use ServiceProviderTrait;
+}
